b8b197771f6600d5d72a0dcc812960a5cdf48681
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-20 13:17:26 -0500

    Add basic Score.create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :difficulty, only: [:index]
+  resources :score, only: %i[index create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
