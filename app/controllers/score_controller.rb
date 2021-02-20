f81ca9026e4abd3552c18b5e08f6680361457a6e
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-20 13:17:26 -0500

    Add basic Score.create

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,3 +1,10 @@
 class ScoreController < ApplicationController
+  def index
+    scores = Score.all
+    render json: scores
+  end
 
+  def create
+    Score.create(points: params[:points], initials: params[:initials], difficulty_id: params[:difficulty_id])
+  end
 end
