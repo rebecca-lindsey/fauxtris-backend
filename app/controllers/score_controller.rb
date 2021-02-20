0be8c1f54e68a6d3c22dec5f7f3862a2c12d3239
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-20 16:27:41 -0500

    Add functionality to create and destory scores

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -7,4 +7,13 @@ class ScoreController < ApplicationController
   def create
     Score.create(points: params[:points], initials: params[:initials], difficulty_id: params[:difficulty_id])
   end
+
+  def destroy
+    score = Score.find(params[:id])
+    score.destroy
+  end
+
+  def show
+    score = Score.find(params[:id])
+  end
 end
